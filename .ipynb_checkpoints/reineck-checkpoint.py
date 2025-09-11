475d5993f552766195f56de54c745929c4b8d08b
Ronald Adomako <rarqp@mst.edu>
2025-09-11 06:30:32 -0400

    progress linear reg.

---
+++ .ipynb_checkpoints/reineck-checkpoint.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy ad np
+import sklearn
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn import preprocessing
+import sys
+import matplotlib.pyplot as plt
+%matplotlib inline
