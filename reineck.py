8b581b2ba6b2ec8e1a1f79d745b77f067f124184
Ronald Adomako <rarqp@mst.edu>
2025-12-21 16:53:11 -0500

    fig comp update WIP

--- reineck.py
+++ reineck.py
@@ -9,4 +9,17 @@ from sklearn import preprocessing
 import sys
 import matplotlib.pyplot as plt
 %matplotlib inline
+import numpy.random as nr
 
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.linear_model import SGDClassifier, Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import KFold, train_test_split
+
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.cluster import KMeans
+from sklearn.neural_network import MLPRegressor
+#from sklearn.datasets import make_regression
+from sklearn.metrics import mean_squared_error
